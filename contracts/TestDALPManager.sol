d23234600975d6868de971ed68bff5cf5a703bb7
opz <will.shahda@gmail.com>
2020-05-23 16:13:24 -0400

    Expose internal function for testing
    Gives unit tests access to the internal _findBestUpdatedUniswapV2Pair
    function and test the return value.

--- contracts/TestDALPManager.sol
+++ contracts/TestDALPManager.sol
@@ -21,6 +21,18 @@ contract TestDALPManager is DALPManager {
         _addUniswapV2Liquidity(tokenA, tokenB);
     }
 
+    /**
+     * @notice Test the _findBestUpdatedUniswapV2Pair internal function
+     * @notice If the return value of tested function does not match the parameter it will revert
+     * @param testPair The Uniswap v2 pair that should match output
+     */
+    function testFindBestUpdatedUniswapV2Pair(address testPair) external {
+        require(
+            address(_findBestUpdatedUniswapV2Pair()) == testPair,
+            "TestDALPManager/test-failed"
+        );
+    }
+
     function getUniswapV2PairRating(IUniswapV2Pair pair) external view returns (uint) {
         return _getUniswapV2PairRating(pair);
     }
